406bdd6c2037ac7c3cc14165c21362def99a7d6e
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-24 00:16:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1,2] = array[1,2].reverse
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |string|
+    string[2] = "$"
+    string
+  end
+end
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+    end
+  end
+end
